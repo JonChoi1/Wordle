f01620f633ed647bcfafa26ab96af96f4688dcac
Jonathan Choi <jkjchoi3@gmail.com>
2022-06-06 19:13:26 -0500

    added function that takes in guess and changes color

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,44 +1,74 @@
 import java.util.Scanner;
+import java.io.*;
 public class Wordle {
     public static void main(String[] args) {
 
         //create word bank
-        //make user input function
-        //make loop that keeps track of input limit, letter verifier, word verifier
-        Scanner scanner = new Scanner(System.in);
-        String[] pastWord = new String[6];
-        String[] wordBank = {"about","above", "abuse", "actor", "acute", "admit", "adult", "after", "again", "agent", "agree", "ahead", "alarm", "album", "alert", "alike", "alive", "allow", "alone", "along", "alter", "among",
-                "anger", "angle", "angry", "apart", "apple", "apply", "arena", "argue", "arise", "array", "aside", "asset", "audio", "audit", "avoid", "award", "aware", "badly", "baker", "bases", "basic", "basis", "beach", "began", "begin", "begun", "being", "below", "bench", "billy",
-                "birth", "black", "blame", "blind", "block", "blood", "board", "boost", "booth", "bound", "brain", "brand", "bread", "break", "breed", "brief", "bring", "broad", "broke", "brown", "build", "built", "buyer", "cable","calif","carry", "catch", "cause", "chain", "chair",
-                "chart", "chase", "cheap", "check", "chest", "chief", "child", "china", "chose", "civil", "claim", "class","clean","clear", "click", "clock", "close", "coach", "coast", "could", "count", "court", "cover", "craft", "crash","cream", "crime", "cross", "crowd", "crown",
-                "curve", "cycle", "daily", "dance", "dated", "dealt", "death", "debut", "delay", "depth", "doing", "doubt", "dozen", "draft", "drama", "drawn", "dream", "dress", "drill", "drink", "drive", "drove", "dying", "eager", "early", "earth", "eight", "elite", "empty", "enemy",
-                "enjoy", "enter", "entry", "equal", "error", "event", "every", "exact", "exist", "extra", "faith", "false", "fault", "fiber", "field", "fifth", "fifty", "fight", "final","first","fixed","flash","fleet","floor","fluid","focus","force","forth","forty","forum","found","frame",
-                "frank","fraud","fresh","front","fruit","fully","funny","giant","given","glass","globe","going","grace","grade","grand","grant","grass","great","green","gross","group","grown","guard","guess","guest","guide","happy","harry","heart","heavy","hence","henry","horse","hotel","house",
-                "human","ideal","image","index","inner","input","issue","japan","jimmy","joint","jones","judge","known","label","large","laser","later","laugh","layer","learn","lease","least","leave","legal","level","lewis","light","limit","links","lives","local","logic","loose","lower","lucky",
-                "lunch","lying","magic","major","maker","march","maria","match","maybe","mayor","meant","media","metal","might","minor","minus","mixed","model","money","month","moral","motor","mount","mouse","mouth","movie","music","needs","never","newly","night","noise","north","noted","novel",
-                "nurse","occur","ocean","offer","often","order","other","ought","paint","panel","paper","party","peace","peter","phase","phone","photo","piece","pilot","pitch","place","plain","plane","plant","plate","point","pound","power","press","price","pride","prime","print","prior","prize",
-                "proof","proud","prove","queen","quick","quiet","quite","radio","raise","range","rapid","ratio","reach","ready","refer","right","rival","river","robin","roger","roman","rough","round","route","royal","rural","scale","scene","scope","score","sense","serve","seven","shall","shape",
-                "share","sharp","sheet","shelf","shell","shift","shirt","shock","shoot","short","shown","sight","since","sixth","sixty","sized","skill","sleep","slide","small","smart","smile","smith","smoke","solid","solve","sorry","sound","south","space","spare","speak","speed","spend","spent",
-                "split","spoke","sport","staff","stage","stake","stand","start","state","steam","steel","stick","still","stock","stone","stood","store","storm","story","strip","stuck","study","stuff","style","sugar","suite","super","sweet","table","taken","taste","taxes","teach","teeth","terry",
-                "texas","thank","theft","their","theme","there","these","thick","thing","think","third","those","three","threw","throw","tight","times","tired","title","today","topic","total","touch","tough","tower","track","trade","train","treat","trend","trial","tried","tries","truck","truly",
-                "trust","truth","twice","under","undue","union","unity","until","upper","upset","urban","usage","usual","valid","value","video","virus","visit","vital","voice","waste","watch","water","wheel","where","which","while","white","whole","whose","woman","women","world","worry","worse",
-                "worst","worth","would","wound","write","wrong","wrote","yield","young","youth"};
+        //make function for user input
+        //make function for word/letter verifier
 
-        int rightWordIndex = (int)(Math.random() * wordBank.length);
+
+        String[] pastGuess = new String[6];
+        String[] wordBank = {"about", "above", "abuse", "actor", "acute", "admit", "adult", "after", "again", "agent", "agree", "ahead", "alarm", "album", "alert", "alike", "alive", "allow", "alone", "along", "alter", "among",
+                "anger", "angle", "angry", "apart", "apple", "apply", "arena", "argue", "arise", "array", "aside", "asset", "audio", "audit", "avoid", "award", "aware", "badly", "baker", "bases", "basic", "basis", "beach", "began", "begin", "begun", "being", "below", "bench", "billy",
+                "birth", "black", "blame", "blind", "block", "blood", "board", "boost", "booth", "bound", "brain", "brand", "bread", "break", "breed", "brief", "bring", "broad", "broke", "brown", "build", "built", "buyer", "cable", "calif", "carry", "catch", "cause", "chain", "chair",
+                "chart", "chase", "cheap", "check", "chest", "chief", "child", "china", "chose", "civil", "claim", "class", "clean", "clear", "click", "clock", "close", "coach", "coast", "could", "count", "court", "cover", "craft", "crash", "cream", "crime", "cross", "crowd", "crown",
+                "curve", "cycle", "daily", "dance", "dated", "dealt", "death", "debut", "delay", "depth", "doing", "doubt", "dozen", "draft", "drama", "drawn", "dream", "dress", "drill", "drink", "drive", "drove", "dying", "eager", "early", "earth", "eight", "elite", "empty", "enemy",
+                "enjoy", "enter", "entry", "equal", "error", "event", "every", "exact", "exist", "extra", "faith", "false", "fault", "fiber", "field", "fifth", "fifty", "fight", "final", "first", "fixed", "flash", "fleet", "floor", "fluid", "focus", "force", "forth", "forty", "forum", "found", "frame",
+                "frank", "fraud", "fresh", "front", "fruit", "fully", "funny", "giant", "given", "glass", "globe", "going", "grace", "grade", "grand", "grant", "grass", "great", "green", "gross", "group", "grown", "guard", "guess", "guest", "guide", "happy", "harry", "heart", "heavy", "hence", "henry", "horse", "hotel", "house",
+                "human", "ideal", "image", "index", "inner", "input", "issue", "japan", "jimmy", "joint", "jones", "judge", "known", "label", "large", "laser", "later", "laugh", "layer", "learn", "lease", "least", "leave", "legal", "level", "lewis", "light", "limit", "links", "lives", "local", "logic", "loose", "lower", "lucky",
+                "lunch", "lying", "magic", "major", "maker", "march", "maria", "match", "maybe", "mayor", "meant", "media", "metal", "might", "minor", "minus", "mixed", "model", "money", "month", "moral", "motor", "mount", "mouse", "mouth", "movie", "music", "needs", "never", "newly", "night", "noise", "north", "noted", "novel",
+                "nurse", "occur", "ocean", "offer", "often", "order", "other", "ought", "paint", "panel", "paper", "party", "peace", "peter", "phase", "phone", "photo", "piece", "pilot", "pitch", "place", "plain", "plane", "plant", "plate", "point", "pound", "power", "press", "price", "pride", "prime", "print", "prior", "prize",
+                "proof", "proud", "prove", "queen", "quick", "quiet", "quite", "radio", "raise", "range", "rapid", "ratio", "reach", "ready", "refer", "right", "rival", "river", "robin", "roger", "roman", "rough", "round", "route", "royal", "rural", "scale", "scene", "scope", "score", "sense", "serve", "seven", "shall", "shape",
+                "share", "sharp", "sheet", "shelf", "shell", "shift", "shirt", "shock", "shoot", "short", "shown", "sight", "since", "sixth", "sixty", "sized", "skill", "sleep", "slide", "small", "smart", "smile", "smith", "smoke", "solid", "solve", "sorry", "sound", "south", "space", "spare", "speak", "speed", "spend", "spent",
+                "split", "spoke", "sport", "staff", "stage", "stake", "stand", "start", "state", "steam", "steel", "stick", "still", "stock", "stone", "stood", "store", "storm", "story", "strip", "stuck", "study", "stuff", "style", "sugar", "suite", "super", "sweet", "table", "taken", "taste", "taxes", "teach", "teeth", "terry",
+                "texas", "thank", "theft", "their", "theme", "there", "these", "thick", "thing", "think", "third", "those", "three", "threw", "throw", "tight", "times", "tired", "title", "today", "topic", "total", "touch", "tough", "tower", "track", "trade", "train", "treat", "trend", "trial", "tried", "tries", "truck", "truly",
+                "trust", "truth", "twice", "under", "undue", "union", "unity", "until", "upper", "upset", "urban", "usage", "usual", "valid", "value", "video", "virus", "visit", "vital", "voice", "waste", "watch", "water", "wheel", "where", "which", "while", "white", "whole", "whose", "woman", "women", "world", "worry", "worse",
+                "worst", "worth", "would", "wound", "write", "wrong", "wrote", "yield", "young", "youth"};
+
+        int rightWordIndex = (int) (Math.random() * wordBank.length);
         String rightWord = wordBank[rightWordIndex];
         System.out.println(rightWord);
-        while (1 == 1){
-            String guess = scanner.nextLine();
-            if (guess.equals(wordBank)){
-                System.out.println(guess);
-            }
+        for (int i = 0; i < 6; i++) {
+            pastGuess[i] += guess(rightWord, wordBank);
+            System.out.println(pastGuess);
 
         }
 
     }
-    //public static String letterVerifer(String wordGuess) {
 
-    //}
+    public static final char ANSI_RESET = "\u001B[0m";
+    public static final char ANSI_GREEN = "\u001B[33m";
+    public static final char ANSI_YELLOW = "\u001B[33m";
 
+    public static String guess(String rightWord, String[] wordBank) {
+        Scanner scanner = new Scanner(System.in);
+        String guess = scanner.nextLine();
+        String guessResult = guess;
+        if (guess.equals(rightWord)) {
+            System.out.println("you win!");
+        } else {
+            //checking if guess a valid word
+            for (int m = 0; m < wordBank.length; m++) {
+                if (guess.equals(wordBank[m])) {
+                    //checking for yellow letters
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < 5; j++) {
+                            if (guess.charAt(i) == (rightWord.charAt(j))){
+                                guess.charAt(i) = ANSI_YELLOW + guess.charAt(i) + ANSI_RESET;
 
+                            }
+                        }
+                    }
+                    //checking for individual greens
+                    for (int n = 0; n < 5; n++){
+                        if (guess.charAt(n) == (rightWord.charAt(n))){
+                            guess.charAt(n) = ANSI_GREEN + guess.charAt(n) + ANSI_RESET;
+                        }
+                    }
+                }
+            }
+        }
+    return guess;
+    }
 }
